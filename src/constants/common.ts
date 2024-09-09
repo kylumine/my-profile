7767127b97e682d03ba289b6dea18ade0b43deca
kylumine <153485467+kylumine@users.noreply.github.com>
2024-09-09 08:06:47 +0800

    Update common.ts

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -3,10 +3,10 @@ export const NAME = 'Fiona R. Millan';
 // ABOUT PAGE
 export const ABOUT_TEXT_1 =
   'A highly motivated student developer with hands-on experience in Laravel, Tailwind CSS, and API development, eager to apply technical skills to real-world projects while continuously learning and growing in the field of web development.';
-export const ABOUT_TEXT_2 =
-  'Responsible for developing and maintaining web applications from the frontend to the backend, work closely with cross-functional teams to design, implement, and deploy scalable solutions that meet business objectives.';
-export const ABOUT_TEXT_3 =
-  'Versatile professional proficient in both frontend and backend development, capable of building complete web applications from start to finish. Possess a strong understanding of various programming languages, frameworks, and databases.';
+// export const ABOUT_TEXT_2 =
+//   'Responsible for developing and maintaining web applications from the frontend to the backend, work closely with cross-functional teams to design, implement, and deploy scalable solutions that meet business objectives.';
+// export const ABOUT_TEXT_3 =
+//   'Versatile professional proficient in both frontend and backend development, capable of building complete web applications from start to finish. Possess a strong understanding of various programming languages, frameworks, and databases.';
 
 // RESUME PAGE
 export const RESUME_TEXT_1 =
